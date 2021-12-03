356e109d8497b22fbb1ae39f3ce1fcc7f9f30946
liupu14 <lpgauss@163.com>
2021-12-03 17:32:54 +0800

    add backend java to respo

---
+++ blog/backend/src/main/java/com/github/liupu14/backend/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.github.liupu14.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "Hello Spring Boot!";
+    }
+}
