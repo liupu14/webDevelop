25862e575ed6dcb6095db4b636de44c44a7024ba
liupu14 <lpgauss@163.com>
2021-12-13 16:08:17 +0800

    adjust flaskblog and add user test

--- Flask/Blog/app/__init__.py
+++ Flask/Blog/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "13f9d18375c494a4561aff0a0fd2d66d"
@@ -8,5 +8,6 @@ app.config["SECRET_KEY"] = "13f9d18375c494a4561aff0a0fd2d66d"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bp = Bcrypt(app)
 
 from app import routes
--- Flask/Blog/app/routes.py
+++ Flask/Blog/app/routes.py
@@ -1,35 +1,46 @@
-from flask import render_template,url_for,flash,redirect
+from flask import render_template, url_for, flash, redirect
 from app.forms import RegistrationForm, LoginForm
 from app import app
 from app.models import User, Post
+from app import db, bp
+
 
 @app.route("/home")
 @app.route("/")
 def home():
-    return render_template("home.html",title="home")
+    return render_template("home.html", title="home")
+
 
 @app.route("/classifier")
 def classifier():
-    return render_template("classifier.html",title="classifier")
+    return render_template("classifier.html", title="classifier")
+
 
 @app.route("/tags")
 def tags():
-    return render_template("tags.html",title="tags")
+    return render_template("tags.html", title="tags")
+
 
 @app.route("/archives")
 def archives():
-    return render_template("archives.html",title="archives")
+    return render_template("archives.html", title="archives")
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html",title="about")
+    return render_template("about.html", title="about")
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        password_hs = bp.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data,email=form.email.data,password=password_hs)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -37,7 +48,8 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bp.check_password_hash(user.password,form.password.data):
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
