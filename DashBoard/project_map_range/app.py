1936115d7aec4da7a57c98ba81dc52059e233772
liupu14 <lpgauss@163.com>
2021-12-20 17:30:49 +0800

    adjust streamlit app.py and add config.toml

--- DashBoard/project_map_range/app.py
+++ DashBoard/project_map_range/app.py
@@ -1,11 +1,15 @@
 import streamlit as st 
 from streamlit_folium import folium_static
 import folium
+import pandas as pd 
 
 st.set_page_config(layout="wide")
 "# streamlit-folium"
 
 file = st.file_uploader("Pick a file")
+if file:
+    data = pd.read_excel(file)
+    st.table(data.head())
 
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
 
@@ -16,4 +20,4 @@ folium.Marker(
 ).add_to(m)
 
 # call to render Folium map in Streamlit
-folium_static(m,width=1200)
+folium_static(m,width=1200,height=500)
